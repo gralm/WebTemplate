213f1f77ca60435dafa366521dc7d4ad7eada553
Mattias <kfmalm@gmail.com>
2021-02-28 00:43:05 +0100

    Initial commit

---
+++ backend/src/main/java/se/hillerstadhill/backend/controller/WebsocketController.java
@@ -0,0 +1,20 @@
+package se.hillerstadhill.backend.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+/*
+Trying example from
+https://www.javainuse.com/spring/boot-websocket
+Using Websockets without stomp i think
+ */
+
+@Controller
+public class WebsocketController {
+    @MessageMapping("/hello")
+    public String greeting(String message) throws InterruptedException {
+        Thread.sleep(500);
+        System.out.println("Tog emot: " + message);
+        return "Greeting";
+    }
+}
